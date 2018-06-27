ea8473fcae18bf6fda30a93d50dfedbabe6a0b13
Ryan Manchester <ryanm17@gmail.com>
2018-06-27 15:50:49 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
 end
