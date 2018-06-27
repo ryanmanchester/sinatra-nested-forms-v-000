96868a7b54b21b9df7e00d59c720fdf921a5a523
Ryan Manchester <ryanm17@gmail.com>
2018-06-27 17:11:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,37 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      #binding.pry
+      pirate_name = params["pirate"]["name"]
+      pirate_weight = params["pirate"]["weight"]
+      pirate_height = params["pirate"]["height"]
+      @pirate = Pirate.new(pirate_name, pirate_weight, pirate_height)
+
+      ship_name_1 = params["pirate"]["ships"][0]["name"]
+      ship_type_1 = params["pirate"]["ships"][0]["type"]
+      ship_booty_1 = params["pirate"]["ships"][0]["booty"]
+      @ship_1 = Ship.new(ship_name_1, ship_type_1, ship_booty_1)
+
+      ship_name_2 = params["pirate"]["ships"][1]["name"]
+      ship_type_2 = params["pirate"]["ships"][1]["type"]
+      ship_booty_2 = params["pirate"]["ships"][1]["booty"]
+      @ship_2 = Ship.new(ship_name_2, ship_type_2, ship_booty_2)
+      erb :'pirates/show'
+
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
